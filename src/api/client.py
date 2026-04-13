9219d8bfe418edbc5c285808ee0831a65af882e8
Michael <michaelkoert@gmail.com>
2026-04-13 15:13:11 -0400

    Feature/better logging (#4)
    * sbom security
    
    * better logging to catch db and api errors

--- src/api/client.py
+++ src/api/client.py
@@ -1,4 +1,7 @@
+import logging
 import requests
+
+logger = logging.getLogger('weather-app.api')
 
 class ApiClient:
     def __init__(self, api_key, base_url):
@@ -7,6 +10,8 @@ class ApiClient:
 
     def fetch_data(self, endpoint):
         url = f"{self.base_url}/{endpoint}&key={self.api_key}&contentType=json"
+        logger.info("Fetching weather data from API for endpoint=%s", endpoint.split('?')[0])
         response = requests.get(url)
+        logger.info("API response status=%s for endpoint=%s", response.status_code, endpoint.split('?')[0])
         response.raise_for_status()
         return response.json()
